7e0ff0d23a58f6bb55ece30f7c3e726e1d6dd1b0
Bladedancerr <lashaghotvadze@gmail.com>
2026-04-01 17:46:47 +0400

    lfs init and updated alphabet tracking logic

--- Assets/Scripts/PointAssigner.cs
+++ Assets/Scripts/PointAssigner.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
@@ -13,16 +14,21 @@ public class PointAssigner : MonoBehaviour
     private int _pointIndex;
 
     [SerializeField]
-    private float _distanceChecker = 100f;
+    private float _distanceChecker = 0;
+    [SerializeField]
+    private float _resetDistanceChecker = 0;
+
+    [SerializeField]
+    private float _maxDistanceBetweenPoints = 0;
+
+    [SerializeField]
+    private List<Vector2> _smoothedPoints;
 
     private Vector2 _touchPos;
 
     private void Start()
     {
-        // for (int i = 0; i < _points.Length; i++)
-        // {
-        //     _lineRenderer.SetPosition(i, _points[i].position);
-        // }
+        GenerateSmoothPoints();
     }
 
     private void Update()
@@ -37,37 +43,83 @@ public class PointAssigner : MonoBehaviour
         }
     }
 
+    private void GenerateSmoothPoints()
+    {
+        _smoothedPoints = new List<Vector2>();
+
+        for (int i = 0; i < _points.Length - 1; i++)
+        {
+            var start = _points[i].position;
+            var end = _points[i + 1].position;
+
+            var distance = Vector2.Distance(start, end);
+
+            int addedPointsAmount = Mathf.CeilToInt(distance / _maxDistanceBetweenPoints);
+
+            for (int j = 0; j < addedPointsAmount; j++)
+            {
+                float t = (float)j / addedPointsAmount;
+                Vector2 newPoint = Vector2.Lerp(start, end, t);
+                _smoothedPoints.Add(newPoint);
+            }
+        }
+
+        _smoothedPoints.Add(_points[_points.Length - 1].position);
+    }
+
     private void CalculateDistanceFromLastPoint(Vector2 pos)
     {
-        if (_pointIndex >= _points.Length)
+        // if all points are done
+        if (_pointIndex >= _smoothedPoints.Count)
         {
             Debug.Log("screen touch sheivsooo kvelaaaaaa");
             return;
         }
 
-        if (Vector2.Distance(_points[_pointIndex].position, pos) < _distanceChecker)
-        {
-            Debug.Log("screen touch axlosaaaaaaaaaaa");
-            _lineRenderer.positionCount = _pointIndex + 1;
-            _lineRenderer.SetPosition(_pointIndex, _points[_pointIndex].position);
-            _pointIndex++;
-        }
-        else if (Vector2.Distance(_points[_pointIndex].position, pos) > 1f)
+        float distToTarget = Vector2.Distance(_smoothedPoints[_pointIndex], pos);
+
+        //finger too far away from last point
+        if (distToTarget > _resetDistanceChecker)
         {
             Debug.Log("screen touch shorsaaaaaa dzaliaaaan");
-            _pointIndex = 0;
-            _lineRenderer.positionCount = 0;
+            ResetLine();
+            return;
         }
+
+        //success
+        if (distToTarget < _distanceChecker)
+        {
+            // Snap the line exactly to your predefined Transform position
+            AddPointToLine(_smoothedPoints[_pointIndex]);
+            _pointIndex++;
+        }
+    }
+
+    private void AddPointToLine(Vector3 position)
+    {
+        _lineRenderer.positionCount++;
+        _lineRenderer.SetPosition(_lineRenderer.positionCount - 1, position);
+    }
+
+    private void ResetLine()
+    {
+        _pointIndex = 0;
+        _lineRenderer.positionCount = 0;
     }
 
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.green;
         Gizmos.DrawWireSphere(_touchPos, _distanceChecker);
-        foreach (var point in _points)
+        if (_smoothedPoints == null)
+        {
+            return;
+        }
+
+        foreach (var point in _smoothedPoints)
         {
             Gizmos.color = Color.red;
-            Gizmos.DrawWireSphere(point.position, 0.1f);
+            Gizmos.DrawWireSphere(point, 0.1f);
         }
     }
 }
